7474cfd3500693f7fdd89b2843e71461dc6b91c3
Note <it.spectre.ru@gmail.com>
2024-09-06 22:26:13 +0300

    Часто задаваемые вопросы"

--- src/navigation.js
+++ src/navigation.js
@@ -41,7 +41,6 @@ export const footerData = {
     { ariaLabel: 'Mail', icon: 'tabler:brand-vk', href: getAsset('/rss.xml') },
   ],
   footNote: `
-    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
     Made by <a class="text-blue-600 underline dark:text-muted" href="https://LuxorWraps.moscow/"> LuxorWraps</a> · All rights reserved.
   `,
 };
